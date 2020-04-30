e565141f7edd06cb4f9a7b5344454f687c18607a
Terinuva <martin.peev@hotmail.com>
2020-04-30 15:45:12 +0300

    Update ising.c
    Ich musste ein paar C++ Sachen noch aus dem Windows code rausschreiben, ich habe SIZE auf GSIZE umbenannt weil es einen Konflikt mit der windows.h library erzeugt hat und einmal fullscreen(0); fullscreen(1); in init() eingefügt weil das Windows terminal sonst Screen overflow Probleme hat.
    
    Ansonsten kompilliert es super.

--- ising.c
+++ ising.c
@@ -1,6 +1,6 @@
 /* Simple 2D Ising simulation
    Written by Robert C. Helling <helling@atdotde.de>
-   
+
    Published under GPL 2.0
 
    Compile as clang -O4 ising.c -lm -o ising
@@ -10,24 +10,96 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-#define SIZE 600
+#define GSIZE 600
 #define PLOTSIZE 80
-#define torus(a) (a < 0 ? a + SIZE : (a >= SIZE ? a - SIZE : a))
+#define torus(a) (a < 0 ? a + GSIZE : (a >= GSIZE ? a - GSIZE : a))
 
-int grid[SIZE][SIZE];
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__)
+#include <windows.h>
+
+/*Turns the cursor on/off*/
+
+void show_console_cursor(_Bool showFlag)
+{
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_CURSOR_INFO     cursorInfo;
+
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.bVisible = showFlag; // set the cursor visibility
+	SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+/*Sets cursor to the top left position to overwrite terminal output*/
+
+void home() {
+	/*Initialise objects for cursor manipulation*/
+	HANDLE hStdout;
+	COORD destCoord;
+	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	/*Sets coordinates of cursor to (0,0) */
+	destCoord.X = 0;
+	destCoord.Y = 0;
+	SetConsoleCursorPosition(hStdout, destCoord);
+
+	show_console_cursor(0);
+}
+
+void fullscreen(_Bool bl) {
+	if (bl) {
+		ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
+	}else if(!bl){
+        ShowWindow(GetConsoleWindow(), SW_RESTORE);
+	}
+}
+
+
+int random() {
+	return rand();
+}
+
+void srandom(unsigned seed){
+	srand(seed);
+}
+
+void system_init() {
+	fullscreen(1);
+	show_console_cursor(0);
+}
+
+#else
+
+void system_init() {
+}
+
+void fullscreen(_Bool bl){
+}
+
+void home() {
+	printf("\033[1;1H");
+}
+#endif
+
+int grid[GSIZE][GSIZE];
 
 void init(){
 	int x,y;
 	FILE *mag;
 
-	mag = fopen("/tmp/magnetization","w");
+	system_init();
+	mag = fopen("magnetization","w");
 	fclose(mag);
 
-	for (x=0; x<SIZE; x++)
-		for (y=0; y<SIZE; y++)
+	for (x=0; x<GSIZE; x++)
+		for (y=0; y<GSIZE; y++)
 			grid[x][y] = random()/(float)RAND_MAX < 0.3  ? -1 : 1;
 
-	srandom(time(NULL));
+ 	fullscreen(0);
+	fullscreen(1);
+
+
+	srandom((int) time(NULL) % (1 << 31));
 }
 
 void output(){
@@ -35,8 +107,9 @@ void output(){
 	int m=0;
 	FILE *mag;
 
-    /* Cursor home */
-	printf("\033[1;1H");
+        /* Cursor home */
+	home();
+
 	for (x=0; x<PLOTSIZE; x++){
 		for (y=0; y<PLOTSIZE*2; y++){
 			if (grid[x][y] == 1)
@@ -47,19 +120,20 @@ void output(){
 		putchar('\n');
 	}
 
-	for (x=0; x<SIZE; x++)
-		for (y=0; y<SIZE; y++)
+
+	for (x=0; x<GSIZE; x++)
+		for (y=0; y<GSIZE; y++)
 			m += grid[x][y];
-	
-	printf("m=%f\n",m/(float)(SIZE * SIZE));
-	mag = fopen("/tmp/magnetization","a");
-	fprintf(mag, "%f\n",m/(float)(SIZE * SIZE));
+
+	printf("m=%f\n",m/(float)(GSIZE * GSIZE));
+	mag = fopen("magnetization","a");
+	fprintf(mag, "%f\n",m/(float)(GSIZE * GSIZE));
 	fclose(mag);
 }
 
 void update(float J, float H){
-	int x = random() * SIZE/RAND_MAX;
-	int y = random() * SIZE/RAND_MAX;
+	int x = (int)(random() * GSIZE/(float)RAND_MAX);
+	int y = (int)(random() * GSIZE/(float)RAND_MAX);
 
 	int sum = grid[x][torus(y+1)] + grid[x][torus(y - 1)] + grid[torus(x + 1)][y] + grid[torus(x - 1)][y];
 
@@ -68,14 +142,14 @@ void update(float J, float H){
 	if ((E < 0.0) || (exp(-E) > (random()/(double)RAND_MAX)))
 		grid[x][y] *= -1;
 }
-		
+
 
 int main(){
 
 	float T = 1.0;
-    float J = 0.44;
+        float J = 0.44;
 	float H = 0.001;
-	
+
 	long t;
 
 	init();
